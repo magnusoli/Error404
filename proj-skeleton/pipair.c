229bef95a9781bd7bcc0ee5546276c7098ac3e58
helgib10 <helgib10@ru.is>
2017-03-08 09:26:21 +0000

    merge

--- proj-skeleton/pipair.c
+++ proj-skeleton/pipair.c
@@ -13,10 +13,10 @@
  * */
 
 int main(int argc, char *argv[]) {
-  printf("bug: A in scope2, pair: (A, B), support: 3, confidence: 75.00%%\n");
-  printf("bug: A in scope3, pair: (A, D), support: 3, confidence: 75.00%%\n");
-  printf("bug: B in scope3, pair: (B, D), support: 4, confidence: 80.00%%\n");
-  printf("bug: D in scope2, pair: (B, D), support: 4, confidence: 80.00%%\n");
+  //printf("bug: A in scope2, pair: (A, B), support: 3, confidence: 75.00%%\n");
+  //printf("bug: A in scope3, pair: (A, D), support: 3, confidence: 75.00%%\n");
+  //printf("bug: B in scope3, pair: (B, D), support: 4, confidence: 80.00%%\n");
+  //printf("bug: D in scope2, pair: (B, D), support: 4, confidence: 80.00%%\n");
 
-  return 0;
+  return 1;
 }
